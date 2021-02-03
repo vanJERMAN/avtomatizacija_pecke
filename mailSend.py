f32a2f63fc41eb6916621f6d9878ea2454b34425
Erik Jerman <vanjermancek@gmail.com>
2021-02-03 17:07:25 +0100

    Update mailSend.py

--- mailSend.py
+++ mailSend.py
@@ -3,7 +3,7 @@ def mail(machine, distance, receiver):
     smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True) # Gmail's SSL port
     smtp.login('HERE GOES SENDER EMAIL', 'HERE GOES SENDER PASSWORD')
     smtp.to(receiver)
-    smtp.write("From: {} <home.automation.seca.179@gmail.com>\n".format(machine))
+    smtp.write("From: {} <HERE GOES SENDER EMAIL>\n".format(machine))
     smtp.write("Subject: Čas je za dofilat pelete!\n\n")
     smtp.write("Kmalu bo zmanjkalo pelet. Razdalja: {}\n".format(distance))
     smtp.send()
